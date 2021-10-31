67f912b9c8588ac12cac93206da09b1d859f542b
NaokiS28 <naokikaito28@gmail.com>
2021-10-31 23:39:28 +0000

    Blank out JVS_Message struct
    Having prefilled data was causing issues so until I can figure out how to do it better, it's blanked out

--- src/jvs_message.h
+++ src/jvs_message.h
@@ -36,8 +36,9 @@ struct JVS_Frame {
 
 typedef enum {
     endCode, switchInput, coinInput, analogInput, rotaryInput, keycodeInput,
-    screenPosInput, miscInput, reserved1, reserved2, cardSlots, medalOutputs, gpOutput,
-    analogOutput, characterOuput, backupSupport
+    screenPosInput, miscInput, reserved1, reserved2, reserved3, reserved4,
+    reserved5, reserved6, reserved7, reserved8, cardSlots, medalOutputs, gpOutput,
+    analogOutput, characterOutput, backupSupport
 } featureTypes;
 
 typedef enum {
@@ -51,15 +52,15 @@ struct JVS_Info {
     uint8_t cmdRev = 11;    // Some JVS hosts (Triforce) will report ver 0.0 if it's higher than 1.1
     uint8_t jvsRev = 30;
     uint8_t comRev = 10;
-    uint8_t totalFeatures = 4;
+    uint8_t totalFeatures = 0;
     featureTypes featureSupport[16] = {
-        switchInput, coinInput, analogInput, gpOutput
+        
     };
     uint8_t featureParameters[16][3] = {
-        {2, 12, 0},     // Is switchInput in generic construction
-        {2, 0, 0},      // 2 Coin slots
-        {6, 10, 0},     // 6 ADCs, 10-Bit resolution
-        {8, 0, 0}       // 8 GPOs (general purpose output)
+        //{2, 12, 0},     // Is switchInput in generic construction
+        //{2, 0, 0},      // 2 Coin slots
+        //{6, 10, 0},     // 6 ADCs, 10-Bit resolution
+        //{8, 0, 0}       // 8 GPOs (general purpose output)
     };
 };
 
